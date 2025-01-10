6620d5743c4bdc71d7adaee7b7cceec1fc3032ef
beeX <beeXgit@gmail.com>
2025-01-10 11:51:26 +0100

    added word count function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        word_list = file_contents.split()
+        word_count = 0
+        for word in word_list:
+            word_count += 1
+        print(word_count)
+
+main()
