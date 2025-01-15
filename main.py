aad3106371503793cce9b64dded48bf0b7029ac7
beeX <beeXgit@gmail.com>
2025-01-15 10:46:36 +0100

    before trying to add sys arguments

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+from operator import itemgetter
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         word_count = get_word_count(file_contents)
         character_counts = get_character_counts(file_contents)
         edited_character_counts = remove_symbols(character_counts)
-        print(edited_character_counts)
         list_of_dicts = get_list_of_dicts(edited_character_counts)
-        print(list_of_dicts)
         sorted_list = sort_list(list_of_dicts)
         print(sorted_list)
+        print(f"======Begin book report of {f}")
+        for i in sorted_list:
+            print(f"The letter {i[0]} appears {i[1]} times.")
 
         
 def get_word_count(file_contents):
@@ -53,8 +56,6 @@ def remove_symbols(character_counts):
 def get_list_of_dicts(edited_character_counts):
     list_of_dicts = list(edited_character_counts.items())
     return list_of_dicts
-
-from operator import itemgetter
 
 def sort_list(list_of_dicts):
     list_of_dicts.sort(reverse=True, key=itemgetter(1))
